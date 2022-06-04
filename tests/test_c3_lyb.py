144d34bea7772a0a682480c098ada26c8430f9db
James Trousdale <james.trousdale@lyondellbasell.com>
2022-06-04 08:46:55 -0500

    Initial implementation (WIP)

--- tests/test_c3_lyb.py
+++ tests/test_c3_lyb.py
@@ -1,64 +1,81 @@
 # -*- coding: utf-8 -*-
 
 
-def test_bar_fixture(testdir):
+def test_c3_fixture(testdir):
     """Make sure that pytest accepts our fixture."""
 
     # create a temporary pytest test module
     testdir.makepyfile("""
-        def test_sth(bar):
-            assert bar == "europython2015"
+        def test_c3_my_user(c3: object):
+            assert c3.User.myUser().id == 'BA'
     """)
 
     # run pytest with the following cmd args
     result = testdir.runpytest(
-        '--foo=europython2015',
+        '--user=BA',
+        '--password=BA',
+        '--tenant=c3',
+        '--tag=c3',
         '-v'
     )
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        '*::test_sth PASSED*',
+        '*::test_c3_my_user PASSED*',
     ])
 
     # make sure that that we get a '0' exit code for the testsuite
     assert result.ret == 0
 
 
-def test_help_message(testdir):
+def test_c3_help_message(testdir):
+
+    # create a temporary pytest test module
+    testdir.makepyfile("""
+        def test_c3_my_user(c3: object):
+            assert c3.User.myUser().id == 'BA'
+    """)
+
     result = testdir.runpytest(
+        # '-v',
+        # '--fixtures',
         '--help',
     )
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines([
-        'c3-lyb:',
-        '*--foo=DEST_FOO*Set the value for the fixture "bar".',
-    ])
 
-
-def test_hello_ini_setting(testdir):
-    testdir.makeini("""
-        [pytest]
-        HELLO = world
-    """)
-
-    testdir.makepyfile("""
-        import pytest
-
-        @pytest.fixture
-        def hello(request):
-            return request.config.getini('HELLO')
-
-        def test_hello_world(hello):
-            assert hello == 'world'
-    """)
-
-    result = testdir.runpytest('-v')
+    print(result.stdout)
+    assert False
 
     # fnmatch_lines does an assertion internally
     result.stdout.fnmatch_lines([
-        '*::test_hello_world PASSED*',
+        '*c3-lyb*',
+        # 'c3-lyb:',
+        # '*--foo=DEST_FOO*Set the value for the fixture "bar".',
     ])
 
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
+
+# def test_hello_ini_setting(testdir):
+#     testdir.makeini("""
+#         [pytest]
+#         HELLO = world
+#     """)
+
+#     testdir.makepyfile("""
+#         import pytest
+
+#         @pytest.fixture
+#         def hello(request):
+#             return request.config.getini('HELLO')
+
+#         def test_hello_world(hello):
+#             assert hello == 'world'
+#     """)
+
+#     result = testdir.runpytest('-v')
+
+#     # fnmatch_lines does an assertion internally
+#     result.stdout.fnmatch_lines([
+#         '*::test_hello_world PASSED*',
+#     ])
+
+#     # make sure that that we get a '0' exit code for the testsuite
+#     assert result.ret == 0
